3e169c1aaff7df985294909fed732974a5a276da
Antonio Serna <nokturn_a@hotmail.com>
2017-05-05 23:23:27 +0200

    Post ver.1
    Primer acercamiento a la vista del post del articulo

---
+++ post.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>TuTecnoMundo - Apps y Juegos Android - Noticias android y top de las mejores apps, juegos y personalizacion para android. Saca el máximo a tu movil / celular o tablet en TuTecnoMundo.</title>
+		<link rel="stylesheet" href="assets/css/main.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>   
+	</head>
+  <body>
+      
+	<div class="wrapper">
+        
+        <?php include 'header.php'; ?>
+        <?php include 'navbar.php'; ?>
+		
+
+	<main id="main">
+		<div style="max-width:1200px" class="container">
+			<div class="row">
+				<div style="max-height: 80vh;" class="col-md-12">
+					<a style="
+						display: block;
+						position: relative;
+						width: 100%;
+						height: 0;
+						padding: 46.66666667% 0 0 0;
+						overflow: hidden;" 
+						href="#">
+						  <img style="
+							position: absolute;
+							display: block;
+							width: 100%;
+							height: 100%;
+							overflow: hidden;
+							object-fit: cover;
+							left: 0;
+							right: 0;
+							top: 0;
+							bottom: 0;
+							margin: auto;" 
+							alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<!-- SIDEBAR NOTICIA -->
+				<div style="background:; padding:2rem;" class="col-md-4">
+					<div style="
+						display: flex;
+						justify-content: flex-end;
+						align-items: center;"
+						class="info-autor">
+						<div style="
+							padding-right: 2rem;">
+							<p style="text-align:right;">Autor</p>
+							<p>Antonio Jesus Serna</p>
+						</div>
+						<div>
+							<a href="#">
+							  <img style="border-radius: 50%;" class="media-object" src="https://www.placecage.com/55/55" alt="...">
+							</a>
+						</div>
+					</div>
+					<div style="
+						display: flex;
+						justify-content: flex-end;
+						padding-top:2rem;
+						align-items: center;"
+						class="info-autor">
+						<div style="
+							padding-right: 2rem;">
+							<p>Leido por 26.793 personas</p>
+						</div>
+						<div>
+							<a href="#">
+							  <img style="border-radius: 50%;" class="media-object" src="https://www.placecage.com/55/55" alt="...">
+							</a>
+						</div>
+					</div>
+					<div style="
+						display: flex;
+						justify-content: flex-end;
+						align-items: center;
+						padding-top:2rem;"
+						class="info-autor">
+						<div style="
+							padding-right: 2rem;">
+							<p>Comentado por 2 personas</p>
+						</div>
+						<div>
+							<a href="#">
+							  <img style="border-radius: 50%;" class="media-object" src="https://www.placecage.com/55/55" alt="...">
+							</a>
+						</div>
+					</div>
+					<hr align="center" width="50%">
+					<div style="
+						display: flex;
+						justify-content: flex-end;
+						align-items: center;
+						padding-top:4rem;"
+						class="info-autor">
+						<div style="
+							padding-right: 2rem;">
+							<p>Comentado por 2 personas</p>
+						</div>
+						<div>
+							<a href="#">
+							  <img style="border-radius: 50%;" class="media-object" src="https://www.placecage.com/55/55" alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+				
+				<!-- CONTENEDOR NOTICIA -->
+				<div style="background:; padding:2rem;" class="col-md-8">
+					<!-- BANNER PUBLI -->
+					<div style="background:black; height:150px; width:100%;"></div>
+					<!-- BREADCRUMBS -->
+					<div style="padding:20px 0 20px 0rem;" >Publicado en TuTecnoMundo > Samsung > Galaxy S7</div>
+					<!-- CUERPO NOTICIA -->
+					<div>
+					<p>Una de las novedades más destacadas del nuevo Galaxy S7 es la protección IP68. Esta
+					acreditación nos garantiza una protección muy fuerte contra el polvo (lo otorga el número
+					6) y una inmersión completa y continuada en agua (lo otorga el número 8), que nos
+					permite mantener el smartphone hasta 30 minutos bajo 1 metro de profundidad sin que
+					este sufra daño alguno.</p>
+					<h2>¿Qué es exactamente la protección ofrecida que ofrece el nuevo Samsung Galaxy S7?</h2>
+					<p>Esta protección nos da un plus de seguridad para todos aquellos que queramos sumergir el
+					smartphone, pues sabemos que Samsung ha trabajado en ello y que en principio no
+					debería ocurrir nada (la acreditación certtifica el aprobado en pruebas de este tipo), pero tal
+					y como conocimos hace escasas horas, Sony ha optado por no incluir en la garantía
+					cualquier defecto en este ámbito, pese haber pasado los test de reconocimiento de
+					acreditación IP68, de modo que si nuestro smartphone se rompiera al ser sumergido, no
+					nos lo cubriría la garantía.</p>
+					<img style="width:100%; padding:20px 0 20px 0rem;" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+					<p>Por el momento, no tenemos constancia de que Samsung haya optado por este camino,
+					sino todo lo contrario, pues en todo momento la empresa ha hecho especial hincapié en
+					este punto, añadiendo que el trabajo realizado ha sido para poder ofrecer a los usuarios un
+					smartphone más completo. Sería un total desastre que finalmente Samsung optara por
+					tomar el camino que ha decidido tomar Sony y no cubrir en la garantía del smartphone un
+					aspecto tan elemental como este, pues entonces no tendría sentido alguno poder usar esta
+					funcionalidad si existe un riesgo real a que algo salga mal.</p>
+					</div>
+					<!-- BANNER PUBLI 2 -->
+					<div style="background:black; height:150px; width:100%;"></div>
+					<h2>Recomendado por el autor</h2>
+					<div class="row">
+						<div class="col-md-4">
+							<p>Titulo de noticia</p>
+							<a style="
+								display: block;
+								position: relative;
+								width: 100%;
+								height: 0;
+								padding: 46.66666667% 0 0 0;
+								overflow: hidden;" 
+								href="#">
+								  <img style="
+									position: absolute;
+									display: block;
+									width: 100%;
+									height: 100%;
+									overflow: hidden;
+									object-fit: cover;
+									left: 0;
+									right: 0;
+									top: 0;
+									bottom: 0;
+									margin: auto;" 
+									alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<p>Titulo de noticia</p>
+							<a style="
+								display: block;
+								position: relative;
+								width: 100%;
+								height: 0;
+								padding: 46.66666667% 0 0 0;
+								overflow: hidden;" 
+								href="#">
+								  <img style="
+									position: absolute;
+									display: block;
+									width: 100%;
+									height: 100%;
+									overflow: hidden;
+									object-fit: cover;
+									left: 0;
+									right: 0;
+									top: 0;
+									bottom: 0;
+									margin: auto;" 
+									alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<p>Titulo de noticia</p>
+							<a style="
+								display: block;
+								position: relative;
+								width: 100%;
+								height: 0;
+								padding: 46.66666667% 0 0 0;
+								overflow: hidden;" 
+								href="#">
+								  <img style="
+									position: absolute;
+									display: block;
+									width: 100%;
+									height: 100%;
+									overflow: hidden;
+									object-fit: cover;
+									left: 0;
+									right: 0;
+									top: 0;
+									bottom: 0;
+									margin: auto;" 
+									alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+							</a>
+						</div>
+					</div>
+					<div style="border: 1px solid #CFD8DC; height: 1px; margin: 30px 0; width: 100%;" class="separador"></div>
+					<div class="row">
+						<div class="col-md-4">
+							<p>Titulo de noticia</p>
+							<a style="
+								display: block;
+								position: relative;
+								width: 100%;
+								height: 0;
+								padding: 46.66666667% 0 0 0;
+								overflow: hidden;" 
+								href="#">
+								  <img style="
+									position: absolute;
+									display: block;
+									width: 100%;
+									height: 100%;
+									overflow: hidden;
+									object-fit: cover;
+									left: 0;
+									right: 0;
+									top: 0;
+									bottom: 0;
+									margin: auto;" 
+									alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<p>Titulo de noticia</p>
+							<a style="
+								display: block;
+								position: relative;
+								width: 100%;
+								height: 0;
+								padding: 46.66666667% 0 0 0;
+								overflow: hidden;" 
+								href="#">
+								  <img style="
+									position: absolute;
+									display: block;
+									width: 100%;
+									height: 100%;
+									overflow: hidden;
+									object-fit: cover;
+									left: 0;
+									right: 0;
+									top: 0;
+									bottom: 0;
+									margin: auto;" 
+									alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+							</a>
+						</div>
+						<div class="col-md-4">
+							<p>Titulo de noticia</p>
+							<a style="
+								display: block;
+								position: relative;
+								width: 100%;
+								height: 0;
+								padding: 46.66666667% 0 0 0;
+								overflow: hidden;" 
+								href="#">
+								  <img style="
+									position: absolute;
+									display: block;
+									width: 100%;
+									height: 100%;
+									overflow: hidden;
+									object-fit: cover;
+									left: 0;
+									right: 0;
+									top: 0;
+									bottom: 0;
+									margin: auto;" 
+									alt="Prueba" src="https://www.tutecnomundo.com/wp-content/uploads/2017/04/xiaomi-mi-6-precio.jpg"/>
+							</a>
+						</div>
+					</div>
+					<br><br>
+					<div style="background:black; height:150px; width:100%;"></div>
+				</div>
+			</div>
+		</div>
+	</main>
+
+		<?php include 'sidebar.php'; ?>
+
+		</div>
+
+
+		<script type="text/javascript" src="assets/js/jquery.js"></script>
+  </body>
+</html>
