1d3aa8e3be29f57cfe6e6f3a46fb8ed8aaaf7f86
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2017-05-05 21:16:11 +0200

    division includes

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>TuTecnoMundo - Apps y Juegos Android - Noticias android y top de las mejores apps, juegos y personalizacion para android. Saca el máximo a tu movil / celular o tablet en TuTecnoMundo.</title>
+		<link rel="stylesheet" href="assets/css/main.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>   
+	</head>
+  <body>
+      
+	<div class="wrapper">
+        
+        <?php include 'header.php'; ?>
+        <?php include 'navbar1.php'; ?>
+		
+
+    <main id="main">
+			<div class="container-foo" style="with: 100%;">
+				<div class="section group">
+					<div class="col s3">
+						<div class="col s3 pl2 pt2" style="height: 200px;">
+							<header style="background-color: white; height: 50px;" class="normal flex justify-center items-center">¿Por qué no triunfan las tablets
+Android?</header>
+							<section style="height: 150px; background-image: url(assets/img/articulos/1.jpeg); background-position: center; background-size: cover; background-repeat: none;"></section>
+						</div>
+						<div class="col s3 pl2 pt2" style="height: 200px;">
+							<header style="background-color: white; height: 50px;" class="normal flex justify-center items-center">WhatsApp podría suspender a
+usuarios insoportables</header>
+							<section style="height: 150px; background-image: url(assets/img/articulos/2.jpg); background-position: center; background-size: cover; background-repeat: none;"></section>
+						</div>
+						<div class="col s3 pl2 pt2" style="height: 200px;">
+							<header style="background-color: white; height: 50px;" class="normal flex justify-center items-center">¿Debería ser Internet gratuito para
+todos?</header>
+							<section style="height: 150px; background-image: url(assets/img/articulos/3.jpg); background-position: center; background-size: cover; background-repeat: none;"></section>
+						</div>
+						<div class="col s3 pl2 pt2" style="height: 200px;">
+							<header style="background-color: white; height: 50px;" class="normal flex justify-center items-center">100 Mejores Juegos para Android del
+2016</header>
+							<section style="height: 150px; background-image: url(assets/img/articulos/4.jpg); background-position: center; background-size: cover; background-repeat: none;"></section>
+						</div>
+					</div>
+					<div class="col s3">
+						<div class="col s6 p2" style="height: 400px;">
+							<section style="background-color: black; height: 400px;"></section></div>
+					</div>
+				</div>
+			</div>
+		</main>
+
+		<?php include 'navbar2.php'; ?>
+
+		</div>
+
+
+		<script type="text/javascript" src="assets/js/jquery.js"></script>
+  </body>
+</html>
---
+++ navbar2.php
@@ -0,0 +1,45 @@
+<aside id="sidebar-right" class="sidebar flex justify-start items-start bgcolor-sidebar">
+			<section>
+				<a href="#" class="categoria-sidebar-right bgcolor-tutecnomundo">
+					<div class="shape-invertido bgcolor-tutecnomundo">Síguenos en Redes</div>
+				</a>
+				<div class="redes">
+					<div class="red"><div class="red-cifra bgcolor-youtube">1M</div><div class="red-youtube bgcolor-youtube"></div><div class="red-text"><a href="#" class="textcolor-youtube">YouTube</a></div></div>
+					<div class="red"><div class="red-cifra bgcolor-facebook">75 K</div><div class="red-facebook bgcolor-facebook"></div><div class="red-text"><a href="#" class="textcolor-facebook">Facebook</a></div></div>
+					<div class="red"><div class="red-cifra bgcolor-google">17 K</div><div class="red-google bgcolor-google"></div><div class="red-text"><a href="#" class="textcolor-google">Google+</a></div></div>
+					<div class="red"><div class="red-cifra bgcolor-twitter">189 K</div><div class="red-twitter bgcolor-twitter"></div><div class="red-text"><a href="#" class="textcolor-twitter">Twitter</a></div></div>
+					<div class="red"><div class="red-cifra bgcolor-instagram">30 K</div><div class="red-instagram bgcolor-instagram"></div><div class="red-text"><a href="#" class="textcolor-instagram">Instagram</a></div></div>
+					<div class="red"><div class="red-cifra bgcolor-pinterest">1 K</div><div class="red-pinterest bgcolor-pinterest"></div><div class="red-text"><a href="#" class="textcolor-pinterest">Pinterest</a></div></div>
+				</div>
+				<a href="#" class="categoria-sidebar-right bgcolor-tutecnomundo">
+					<div class="shape-invertido bgcolor-tutecnomundo">Descarga la App</div>
+				</a>
+				<div class="app">
+					<img src="https://placehold.it/250x160" alt="app" />
+					<p>Contenido a la carta gratis de lo que más te gusta.</p>
+				</div>
+				<a href="#" class="categoria-sidebar-right bgcolor-tutecnomundo">
+					<div class="shape-invertido bgcolor-tutecnomundo">Participa en el Foro</div>
+				</a>
+				<div class="participa-foro">
+					<form class="" action="" method="post">
+						<textarea name="name" placeholder="Haz tu pregunta" class="bordercolor-tutecnomundo"></textarea>
+						<div class="participa-foro-botones">
+							<input type="submit" name="name" value="Enviar" class="bgcolor-tutecnomundo textcolor-blanco"/>
+							<input type="button" name="name" value="Ver temas" class="bgcolor-tutecnomundo textcolor-blanco"/>
+						</div>
+					</form>
+
+				</div>
+				<a href="#" class="categoria-sidebar-right bgcolor-tutecnomundo">
+					<div class="shape-invertido bgcolor-tutecnomundo">Lo más buscado</div>
+				</a>
+				<div class="mas-buscado">
+					<span>Aplicaciones</span> <span>Google</span> <span>Samsung</span> <span>Juegos</span> <span>Especiales</span> <span>Google Play</span> <span>Actulizacion</span> <span>Sony</span> <span>Motorola</span> <span>HTC</span> <span>Fotografía</span> <span>Seguridad</span> <span>Root</span> <span>LG</span> <span>Opinión</span> <span>Personalización</span> <span>Tablet</span> <span>Trucos</span> <span>Móviles</span> <span>Nexus</span> <span>Facebook</span> <span>Rumores</span> <span>Filtraciones</span> <span>Ofertas</span> <span>Promociones</span> <span>Batería</span> <span>Desarrollo</span> <span>Whatsapp</span> <span>Musica</span> <span>Tablets</span>
+				</div>
+				<div class="publicidad-sidebar-right">
+					<img src="https://placehold.it/250x600/000/fff" alt="" />
+
+				</div>
+			</section>
+		</aside>
