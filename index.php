f3f4db4deee45e2ef3b1f8b9ec2970e59fa42d0c
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2017-05-15 20:37:11 +0200

    asd

--- index.php
+++ index.php
@@ -14,9 +14,10 @@
                      <div class="col-lg-9 col-md-8 col-sm-12">
                          
                         <section>
-                            <div class="row m2">
+<!--                            <div class="row m2" style="display: flex; flex-direction: column; align-items: center;">-->
+                            <div class="row">
                                 <div class="col-lg-8 col-lg-push-4 col-xs-12">
-                                    <article><br/>
+                                    <article>
                                         <header style="position:absolute; top:10px; right:20px;"><h2><a href="post.php"  style="color:white;">Lorem ipsum dolor sit amet</a></h2></header>
                                         <a href="post.php"><img src="assets/img/articulos/1.jpeg" class="img-responsive"></a>
                                     </article>
@@ -36,7 +37,7 @@
                                 
                             <div style="border: 1px solid #E2E2E2; height: 1px; margin: 30px 0; width: 100%;" class="separador"></div>
                             
-                            <div class="row m2">
+                            <div class="row">
                                 <article class="col-sm-4 col-xs-12">
                                     <header><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
                                         <a href="post.php"><img src="assets/img/articulos/3.jpg" class="img-responsive"></a>
@@ -58,60 +59,7 @@
                                 <div style="background:#e2e2e2; position:relative; top:120px; width:115px; height:30px; padding:5px;">
                                     <i class="fa fa-bullhorn fa-fw" aria-hidden="true"></i>&nbsp; Anúnciate
                                 </div>
-                            </div>                            
-                                
-                            <div style="border: 1px solid #E2E2E2; height: 1px; margin: 30px 0; width: 100%;" class="separador"></div>
-                            
-                        </section>
-                         
-                        <section>
-                            <div class="row m2">
-                                <div class="col-lg-8 col-lg-push-4 col-xs-12">
-                                    <article><br/>
-                                        <header style="position:absolute; top:10px; right:20px;"><h2><a href="post.php"  style="color:white;">Lorem ipsum dolor sit amet</a></h2></header>
-                                        <a href="post.php"><img src="assets/img/articulos/1.jpeg" class="img-responsive"></a>
-                                    </article>
-                                </div>
-
-                                <div class="col-lg-4 col-lg-pull-8 col-xs-12">
-                                    <article class="col-lg-12 col-sm-6 col-xs-12">
-                                        <header><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                        <a href="post.php"><img src="assets/img/articulos/2.jpg" class="img-responsive"></a>
-                                    </article>
-                                    <article class="col-lg-12 col-sm-6 col-xs-12">
-                                        <header><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                        <a href="post.php"><img src="assets/img/articulos/6.jpg" class="img-responsive"></a>
-                                    </article>
-                                </div>
-                            </div>                            
-                                
-                            <div style="border: 1px solid #E2E2E2; height: 1px; margin: 30px 0; width: 100%;" class="separador"></div>
-                            
-                            <div class="row m2">
-                                <article class="col-sm-4 col-xs-12">
-                                    <header><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                        <a href="post.php"><img src="assets/img/articulos/3.jpg" class="img-responsive"></a>
-                                </article>
-                                <article class="col-sm-4 col-xs-12">
-                                    <header><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                        <a href="post.php"><img src="assets/img/articulos/4.jpg" class="img-responsive"></a>
-                                </article>
-                                <article class="col-sm-4 col-xs-12">
-                                    <header><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                        <a href="post.php"><img src="assets/img/articulos/5.jpg" class="img-responsive"></a>
-                                </article>
-                            </div>                            
-                                
-                            <div style="border: 1px solid #E2E2E2; height: 1px; margin: 30px 0; width: 100%;" class="separador"></div>
-                            
-                             <!-- anuncio -->
-                             <div class="" style="background:black; height:150px; width:100%;">
-                                <div style="background:#e2e2e2; position:relative; top:120px; width:115px; height:30px; padding:5px;">
-                                    <i class="fa fa-bullhorn fa-fw" aria-hidden="true"></i>&nbsp; Anúnciate
-                                </div>
-                            </div>                            
-                                
-                            <div style="border: 1px solid #E2E2E2; height: 1px; margin: 30px 0; width: 100%;" class="separador"></div>
+                            </div>
                             
                         </section>
                          
