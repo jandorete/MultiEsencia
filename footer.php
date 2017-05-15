5bc011fce8d1efa25af7d5e0695cd8594e35ca6a
Antonio Serna <nokturn_a@hotmail.com>
2017-05-15 19:06:42 +0200

    Select Text And Share plugin Jquery
    JEJEJJEJE

--- footer.php
+++ footer.php
@@ -1,1 +1,5 @@
+<script src="dist/selection-sharer.js"></script>
+<script>
+$('p').selectionSharer();
+</script>
 <script type="text/javascript" src="assets/js/jquery.js"></script>
--- head.php
+++ head.php
@@ -1,7 +1,10 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <link rel="stylesheet" href="assets/css/main.css">
+<link rel="stylesheet" href="dist/selection-sharer.css" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta property="fb:app_id" content="123456789" />
+<meta property="og:url" content="http://your.url/to/share" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
