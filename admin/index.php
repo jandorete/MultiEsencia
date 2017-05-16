3ace73decea222dd6d25796c17b4a7040aa77b13
Antonio Serna <nokturn_a@hotmail.com>
2017-05-16 09:51:48 +0200

    Primer intento de admin login
    dsa una puta mierda de login

---
+++ admin/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+    <head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>TuTecnoMundo - Apps y Juegos Android - Noticias android y top de las mejores apps, juegos y personalizacion para android. Saca el máximo a tu movil / celular o tablet en TuTecnoMundo.</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<style>
+		body{
+			background-color: #d9af05;
+		}
+		.login-superior {
+			background-color: #f3c600;
+			height:70vh;
+		}
+		.login-inferior {
+			padding-top:3rem;
+			height:30vh;
+		}
+
+		</style>
+    </head>
+    <body>
+		<div class="container-fluid">
+			<div class="login-superior row">
+				<div class="container">
+					<div style="text-align: center; display: block;" class="col-md-12">
+						<img style="width:300px;" src="../assets/img/logo-multiesencia.png"/>
+						<h1 style="color:#fff; font-size:60px;" >Creadores</h1>
+					</div>
+				</div>
+			</div>
+			<div class="login-inferior row">
+				<div class="container">
+					<div class="col-md-12">
+						<form class="form-horizontal">
+						  <div class="form-group">
+							<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+							<div class="col-sm-10">
+							  <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+							</div>
+						  </div>
+						  <div class="form-group">
+							<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+							<div class="col-sm-10">
+							  <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+							</div>
+						  </div>
+						  <div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+							  <div class="checkbox">
+								<label>
+								  <input type="checkbox"> Remember me
+								</label>
+							  </div>
+							</div>
+						  </div>
+						  <div class="form-group">
+							<div class="col-sm-offset-2 col-sm-10">
+							  <button type="submit" class="btn btn-default">Sign in</button>
+							</div>
+						  </div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+    </body>
+</html>
