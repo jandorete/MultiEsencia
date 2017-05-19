7b6beee4d19368dc2c064eb5538288a77a19a1cc
Antonio Serna <nokturn_a@hotmail.com>
2017-05-19 16:25:42 +0200

    404 añadido

--- 404.php
+++ 404.php
@@ -9,100 +9,61 @@
         <div class="container">
             
             <div class="row">
-                <?php include 'navbar.php'; ?>
-                <div class="col-md-8 col-sm-12 content">
-                    <section>
-                        <div class="row grupo-noticias">
-                            <div class="col-lg-8 col-lg-push-4 col-xs-12">
-                                <article>
-                                    <header style="position:absolute; right:20px; max-width:400px;"><h2 class="destacada"><a href="post.php"  style="color:white;">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/7.jpg" class="img-responsive destacada"></a>
-                                </article>
-                            </div>
-                            <div class="col-lg-4 col-lg-pull-8 col-xs-12">
-                                <article class="col-lg-12 col-sm-6 col-xs-12">
-                                    <header><h2 class="titular"><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/2.jpg" class="img-responsive img-noticia"></a>
-                                </article>
-                                <article class="col-lg-12 col-sm-6 col-xs-12">
-                                    <header><h2 class="titular"><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/6.jpg" class="img-responsive"></a>
-                                </article>
-                            </div>
-                        </div>                            
-
+				<div class="col-md-12">
+					<h1>Lo sentimos! <img width=50 src="assets/img/404.svg"/></h1> 
+					<h2 style="font-size:24px;" >La página que buscas no existe</h2><br>
+					<p>Quizá has escrito mal la dirección, o la hemos roto nosotros.</p>
+					<p>Puedes escribir en el siguiente buscador para encontrar lo que quieres:</p><br><br>
+					<h2 style="font-size:24px;" >Buscar en TuTecnoMundo</h2><br>
+					<div id="custom-search-input">
+						<div class="input-group col-md-12">
+							<input type="text" class="form-control input-lg" placeholder="Buscar" />
+							<span class="input-group-btn">
+								<button class="btn btn-info btn-lg" type="button">
+									<i class="glyphicon glyphicon-search"></i>
+								</button>
+							</span>
+						</div>
+					</div>
+					 <section>  
                         <div style="border: 1px solid #E2E2E2; height: 1px; margin: 25px 0; width: 100%;"></div>
-
                         <div class="row grupo-noticias">
-                            <article class="col-sm-4 col-xs-12">
+                            <article class="col-sm-3 col-xs-12">
                                 <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
                                     <a href="post.php"><img src="assets/img/articulos/3.jpg" class="img-responsive"></a>
                             </article>
-                            <article class="col-sm-4 col-xs-12">
+                            <article class="col-sm-3 col-xs-12">
                                 <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
                                     <a href="post.php"><img src="assets/img/articulos/4.jpg" class="img-responsive"></a>
                             </article>
-                            <article class="col-sm-4 col-xs-12">
+                            <article class="col-sm-3 col-xs-12">
+                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
+                                    <a href="post.php"><img src="assets/img/articulos/5.jpg" class="img-responsive"></a>
+                            </article>
+							<article class="col-sm-3 col-xs-12">
+                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
+                                    <a href="post.php"><img src="assets/img/articulos/5.jpg" class="img-responsive"></a>
+                            </article>
+                        </div><div class="row grupo-noticias">
+                            <article class="col-sm-3 col-xs-12">
+                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
+                                    <a href="post.php"><img src="assets/img/articulos/3.jpg" class="img-responsive"></a>
+                            </article>
+                            <article class="col-sm-3 col-xs-12">
+                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
+                                    <a href="post.php"><img src="assets/img/articulos/4.jpg" class="img-responsive"></a>
+                            </article>
+                            <article class="col-sm-3 col-xs-12">
+                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
+                                    <a href="post.php"><img src="assets/img/articulos/5.jpg" class="img-responsive"></a>
+                            </article>
+							<article class="col-sm-3 col-xs-12">
                                 <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
                                     <a href="post.php"><img src="assets/img/articulos/5.jpg" class="img-responsive"></a>
                             </article>
                         </div>
-
-                         <!-- anuncio -->
-                        <div style="background:black; height:150px; width:100%; margin-top:25px; margin-bottom:25px;">
-                            <div style="background:#e2e2e2; position:relative; top:120px; width:115px; height:30px; padding:5px;">
-                                <i class="fa fa-bullhorn fa-fw" aria-hidden="true"></i>&nbsp; Anúnciate
-                            </div>
-                        </div>
                     </section>
-                    
-                    <section>
-                        <div class="row grupo-noticias">
-                            <div class="col-lg-8 col-xs-12">
-                                <article>
-                                    <header style="position:absolute; right:20px; max-width:400px;"><h2 class="destacada"><a href="post.php"  style="color:white;">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/7.jpg" class="img-responsive destacada"></a>
-                                </article>
-                            </div>
-                            <div class="col-lg-4 col-xs-12">
-                                <article class="col-lg-12 col-sm-6 col-xs-12">
-                                    <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/2.jpg" class="img-responsive img-noticia"></a>
-                                </article>
-                                <article class="col-lg-12 col-sm-6 col-xs-12">
-                                    <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/6.jpg" class="img-responsive"></a>
-                                </article>
-                            </div>
-                        </div>                            
-
-                        <div style="border: 1px solid #E2E2E2; height: 1px; margin: 25px 0; width: 100%;"></div>
-
-                        <div class="row grupo-noticias">
-                            <article class="col-sm-4 col-xs-12">
-                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/3.jpg" class="img-responsive"></a>
-                            </article>
-                            <article class="col-sm-4 col-xs-12">
-                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/4.jpg" class="img-responsive"></a>
-                            </article>
-                            <article class="col-sm-4 col-xs-12">
-                                <header class="titular"><h2><a href="post.php">Lorem ipsum dolor sit amet</a></h2></header>
-                                    <a href="post.php"><img src="assets/img/articulos/5.jpg" class="img-responsive"></a>
-                            </article>
-                        </div>
-
-                         <!-- anuncio -->
-                        <div style="background:black; height:150px; width:100%; margin-top:25px; margin-bottom:25px;">
-                            <div style="background:#e2e2e2; position:relative; top:120px; width:115px; height:30px; padding:5px;">
-                                <i class="fa fa-bullhorn fa-fw" aria-hidden="true"></i>&nbsp; Anúnciate
-                            </div>
-                        </div>
-                    </section>
-                    
-                </div>
-                <?php include 'sidebar.php'; ?>
+				</div>
             </div>
         <?php include 'footer.php'; ?>
         </div>
