1399a22565971dad9af76e3107ba1f4709306d34
Antonio Serna <nokturn_a@hotmail.com>
2017-05-27 03:04:45 +0200

    Cambios en el css prioritario del adminpanel

--- admin/head.php
+++ admin/head.php
@@ -5,4 +5,3 @@
 <link rel="shortcut icon" href="assets/img/Favicon.png" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="../assets/css/main.css">
-<link rel="stylesheet" href="assets/css/adminpanel.css">
