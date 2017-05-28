23b3ed7da5870f0974fdf2ab3d9c99ee68163c22
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2017-05-28 18:32:43 +0200

    ajustes admin

--- admin/header.php
+++ admin/header.php
@@ -5,31 +5,31 @@
                         <span class="texto-logo">TuTecnoMundo</span>
                     </a>
                 </li>
-                <li class="nav-link" >
+                <li class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/multiesencia/admin/inicio.php") echo 'seleccion-cabecera'; ?>" >
                     <a href="inicio.php" id="menu-btn">
-                        <i class="fa fa-home fa-lg" aria-hidden="true"></i>&nbsp; 
+                        <i class="fa fa-home fa-lg" aria-hidden="true"></i>
 						<span class="texto-cabecera">Inicio</span>
                     </a>
                 </li>
-				<li class="nav-link" >
+				<li class="nav-link <?php if (strpos($_SERVER['REQUEST_URI'], 'chat') !== false) echo 'seleccion-cabecera'; ?>">
                     <a href="chat/" id="menu-btn">
                         <i class="fa fa-weixin fa-lg" aria-hidden="true"></i>&nbsp; 
 						<span class="texto-cabecera">Chat</span>
                     </a>
                 </li>
-				<li class="nav-link">
+				<li class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/multiesencia/admin/redaccion.php") echo 'seleccion-cabecera'; ?>">
                     <a href="redaccion.php" id="search-btn">
                         <i class="fa fa-newspaper-o fa-lg" aria-hidden="true"></i>&nbsp;
                         <span class="texto-cabecera">Redacción</span>
                     </a>
                 </li>
-				<li class="nav-link">
+				<li class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/multiesencia/admin/perfil.php") echo 'seleccion-cabecera'; ?>">
                     <a href="perfil.php" id="search-btn">
                         <i class="fa fa-user fa-lg" aria-hidden="true"></i>&nbsp;
                         <span class="texto-cabecera">Mi perfil</span>
                     </a>
                 </li>
-                <li class="nav-link">
+                <li class="nav-link <?php if ($_SERVER['REQUEST_URI'] == "/multiesencia/admin/administracion.php") echo 'seleccion-cabecera'; ?>">
                     <a href="administracion.php" id="search-btn">
                         <i class="fa fa-area-chart fa-lg" aria-hidden="true"></i>&nbsp;
                         <span class="texto-cabecera">Administración</span>
